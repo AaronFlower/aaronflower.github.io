e5f9140f4f3894a77f0b1e3fedcf1277c851fdc3
AaronFlower <haojunzhan@gmail.com>
2017-02-13 19:36:12 +0800

    Add css icons fonts and icon component

---
+++ projs/vue-2.0-webpack/src/components/antd/icon/icon.vue
@@ -0,0 +1,32 @@
+<template>
+  <i :class="classes" :style="styles"></i>
+</template>
+
+<script>
+  const iconPrefixer = 'tui-icon'
+
+  export default {
+    props: {
+      type: String,
+      size: [Number, String],
+      color: String
+    },
+
+    computed: {
+      classes () {
+        return `${iconPrefixer} ${iconPrefixer}-${this.type}`
+      },
+      styles () {
+        let style = {}
+        let size = Number.parseInt(this.size)
+        if (size) {
+          style['font-size'] = `${size}`
+        }
+        if (this.color) {
+          style.color = this.color
+        }
+        return style
+      }
+    }
+  }
+</script>
---
+++ projs/vue-2.0-webpack/src/demo/tuiComponents.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="icons-container">
+    <div class="icon-item clearfix">
+      <Icon type="send"></Icon>
+      <p>send</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="github"></Icon>
+      <p>github</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="search"></Icon>
+      <p>search</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="facebook"></Icon>
+      <p>facebook</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="twitter"></Icon>
+      <p>twitter</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="check"></Icon>
+      <p>check</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="expand_less"></Icon>
+      <p>expand_less</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="expand_more"></Icon>
+      <p>expand_more</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="first_page"></Icon>
+      <p>first_page</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="last_page"></Icon>
+      <p>last_page</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="kb_backspace"></Icon>
+      <p>kb_backspace</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="kb_capslock"></Icon>
+      <p>kb_capslock</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="kb_tab"></Icon>
+      <p>kb_tab</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="rotate_left"></Icon>
+      <p>rotate_left</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="rss_feed"></Icon>
+      <p>rss_feed</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="search2"></Icon>
+      <p>search2</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="download"></Icon>
+      <p>download</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="emoji-flirt"></Icon>
+      <p>emoji-flirt</p>
+    </div>
+    <div class="icon-item clearfix">
+      <Icon type="weibo"></Icon>
+      <p>weibo</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Icon from '../components/antd/icon/icon'
+
+  export default {
+    components: {
+      Icon
+    }
+  }
+</script>
+
+<style>
+  .icons-container {
+    & .icon-item {
+      display: inline-block;
+      text-align: center;
+      width: 100px;
+      margin: 5px;
+      & .tui-icon:hover {
+        color: #00BCD4;
+        font-size: 1.1em;
+      }
+    }
+  }
+</style>
